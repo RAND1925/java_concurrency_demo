10fad5fdf99ea7d54e61fb02468ab4d265875e15
rand1925 <RAND1925@outlook.com>
2020-11-04 20:48:15 +0800

    producer consumer model

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,11 @@ package com.company;
 import com.company.util.Util;
 
 public class Main {
-    public static final int times = 20;
-
-    public static void thread() throws InterruptedException {
-        Thread[] threads = new Thread[times];
-        //Counter counter = new Counter();
-        Counter counter = new Counter();
-        for (int i = 0; i < times; ++i) {
-            String si = String.valueOf(i);
-            Thread thread = new Thread(new Task(counter), "thread"+ si);
-            threads[i] = thread;
-            thread.start();
-            // thread.join(); // not like this caz it may
-        }
-        for (Thread thread: threads) {
-            thread.join(1000);
-        }
-
-    }
-
 
     public static void main(String[] args) throws InterruptedException {
-        long begin =  Util.log("begin", "MAIN");
-        thread();
-        long end = Util.log("end", "MAIN");
-
-        Util.log("cost", "MAIN", end-begin);
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
     }
 
 }
