6ce59964fb0d5e8010a403ba54c6aac10ef12bd1
rand1925 <RAND1925@outlook.com>
2020-11-05 19:06:50 +0800

    random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,20 +9,18 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static final int times = 20;
-
-
-
-
     public static void main(String[] args) throws InterruptedException {
         ExecutorService pool = Executors.newFixedThreadPool(10);
         AtomicInteger atomicInteger = new AtomicInteger(0);
+        int integer = 0;
         for (int i= 0; i < 20; ++i) {
-            pool.execute(()->{
-                // after java1.7
-                System.out.println("util " + new Random().nextInt());
-                // better performance
-                System.out.println("thrd " + ThreadLocalRandom.current().nextInt());
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("util " + new Random().nextInt());
+                    // better performance
+                    System.out.println("thrd " + ThreadLocalRandom.current().nextInt());
+                }
             });
         }
     }
