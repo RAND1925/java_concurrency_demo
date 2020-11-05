9633dfe0978578f274337677687b224e9d6b982c
rand1925 <RAND1925@outlook.com>
2020-11-05 18:08:57 +0800

    random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,40 +2,28 @@ package com.company;
 
 import com.company.util.Util;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static final int times = 20;
 
-    private static void runNonConcurrency() {
-        for (int i = 0; i < times; ++i) {
-            String si = String.valueOf(i);
 
-            new Task(si).run();
-        }
-
-    }
-
-    public static void runThread() throws InterruptedException {
-        Thread[] threads = new Thread[times];
-        for (int i = 0; i < times; ++i) {
-            String si = String.valueOf(i);
-            Thread thread = new Thread(new Task(si), "thread"+ si);
-            threads[i] = thread;
-            thread.start();
-            // thread.join(); // not like this caz it may
-        }
-        for (Thread thread: threads) {
-            thread.join();
-        }
-    }
 
 
     public static void main(String[] args) throws InterruptedException {
-        long begin =  Util.log("begin", "MAIN");
-        //runThread();
-        runNonConcurrency();
-        long end = Util.log("end", "MAIN");
-
-        Util.log("cost", "MAIN", end-begin);
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        for (int i= 0; i < 20; ++i) {
+            pool.execute(()->{
+                // after java1.7
+                System.out.println("util " + new Random().nextInt());
+                // better performance
+                System.out.println("thrd " + ThreadLocalRandom.current().nextInt());
+            });
+        }
     }
-
 }
